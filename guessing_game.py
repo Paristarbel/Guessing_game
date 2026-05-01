41695063bbed64d501eab396aed16e08aa91b7e4
Paris Amorita Nyoni <nyoniparis37@gmail.com>
2026-05-01 14:26:13 +0000

    done

--- guessing_game.py
+++ guessing_game.py
@@ -38,7 +38,36 @@ def guess_game(attempt):
                 
         
         return attempt
-    
+    if difficulty == "hard":
+        
+        print(f"You have {attempt-5} attempts remaining to guess the number.")
+        # is_true=True
+        is_hard=True
+        while is_hard:
+            guess_answer=int(input("Make a guess: "))
+            
+            if guess_answer != number_guess:
+            # while guess_answer != number_guess:
+                attempt=attempt-1
+                if guess_answer>number_guess:
+                    print(f"You have {attempt-5} attempts remaining to guess the number.")
+                    print('Too high')
+                
+                if guess_answer < number_guess:
+                    print(f"You have {attempt-5} attempts remaining to guess the number.")
+                    print("Too low")
+                if attempt-5==0:
+                    is_hard =False
+                    print("Unfortunately you lost")
+            if guess_answer==number_guess :
+                print(f"You have {attempt-5} attempts remaining to guess the number.")
+                is_running=False
+                print(f"That's correct the number is {number_guess}")
+        
+       
+
+        return attempt
+            
             
         
 
