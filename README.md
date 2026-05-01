42e002551eede46969982c2ffe3c77789a16cecc
Paris Amorita Nyoni <nyoniparis37@gmail.com>
2026-05-01 14:22:12 +0000

    did the easy level part

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Number-Guessing-Game
-It's a game in which you have to guess a correct number thus you will be given attempts which depend on the level of difficulty .
+It's a game in which you have to guess a correct number thus you will be given a few numbwer of attempts which depend on the level of difficulty .
+Now , you will have 2 difficulties which are easy level and hard level.In easy level you will receive 10 attempts and in hard level you will receive 5 attempts.
+All the best and guess wisely😐.
---
+++ guessing_game.py
@@ -0,0 +1,55 @@
+import random
+
+"""easy
+
+ _   _ _    _ __  __ ____  ______ _____                              _                _____                      
+| \ | | |  | |  \/  |  _ \|  ____|  __ \                            (_)              / ____|       :)              
+|  \| | |  | | \  / | |_) | |__  | |__) |   __ _ _   _  ___  ___ ___ _ _ __   __ _  | |  __  __ _ _ __ ___   ___ :)
+| . ` | |  | | |\/| |  _ <|  __| |  _  /   / _` | | | |/ _ \/ __/ __| | '_ \ / _` | | | |_ |/ _` | '_ ` _ \ / _ \
+| |\  | |__| | |  | | |_) | |____| | \ \  | (_| | |_| |  __/\__ \__ \ | | | | (_| | | |__| | (_| | | | | | |  __/  :)
+|_| \_|\____/|_|  |_|____/|______|_|  \_\  \__, |\__,_|\___||___/___/_|_| |_|\__, |  \_____|\__,_|_| |_| |_|\___|
+                                            __/ |                             __/ |                              
+                                           |___/                             |___/                               
+
+"""
+
+number_guess=random.randint(1,100)
+attempts=10
+def guess_game(attempt):
+    
+    print("Welcome to Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+
+    difficulty=input("Choose,difficulty.Type 'easy' or 'hard':")
+
+    if difficulty == "easy":
+        
+        print(f"You have {attempt} attempts remaining to guess the number.")
+        
+        is_running=True
+        while is_running:
+            guess_answer=int(input("Make a guess: "))
+            if guess_answer==number_guess :
+                print(f"You have {attempt} attempts remaining to guess the number.")
+                return attempt
+            if guess_answer != number_guess:
+                while guess_answer != number_guess:
+                    attempt-=1
+                    if guess_answer>number_guess:
+                        print(f"You have {attempt} attempts remaining to guess the number.")
+                        print('Too high')
+                    
+                    if guess_answer < number_guess:
+                        print(f"You have {attempt} attempts remaining to guess the number.")
+                        print("Too low")
+
+                    return attempt
+                
+                
+            
+
+                
+
+attempts=guess_game(attempts)
+
+
